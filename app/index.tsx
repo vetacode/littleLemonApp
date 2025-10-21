4458623e76342f46a94bf7fde642f932168bfe8e
vetacode <vetacode@gmail.com>
2025-10-21 22:49:14 +0700

    welcome to little lemon header

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
+import * as React from 'react';
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function LittleLemonHeader() {
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        flex: 0.2,
+                backgroundColor: '#F4CE14',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{padding: 40, fontSize: 30, color: 'black'
+      }}>Welcome to Little Lemon</Text>
     </View>
   );
 }
