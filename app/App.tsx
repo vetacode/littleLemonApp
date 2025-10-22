53edfddc95555b6cee4bf037819d7906637afe04
vetacode <vetacode@gmail.com>
2025-10-22 14:02:26 +0700

    Revert "change app to main"
    This reverts commit ce3d8bb9b50d16f1a961ec6f587c2a9e03e57988.

--- app/App.tsx
+++ app/App.tsx
@@ -1,18 +1,12 @@
 import * as React from 'react';
 import { View } from 'react-native';
-import LittleLemonFooter from './LittleLemonFooter';
 import LittleLemonHeader from './LittleLemonHeader';
 
 
-export default function Main () {
+export default function App () {
     return (
-        <>
         <View style={{flex: 1, backgroundColor: '#495E57',}}> 
             <LittleLemonHeader/>
         </View>
-        <View style={{backgroundColor: '#495E57',}}> 
-            <LittleLemonFooter/>
-        </View>
-        </>
     );
 }
