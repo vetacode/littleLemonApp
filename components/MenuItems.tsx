dbe9857399dd8623cbbe22bb0db5864f8b3acef4
vetacode <vetacode@gmail.com>
2025-10-23 17:42:59 +0700

    adding FlatList for MenuItems

--- components/MenuItems.tsx
+++ components/MenuItems.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const green = '#495E57';
 const yellow = '#F4CE14';
@@ -8,14 +8,42 @@ const black = '#333333';
 const white = '#EDEFEE';
 
 const menuItemsToDisplay = [
-  'Hummus \n Moutabal \n Falafel \n Marinated Olives \n Kofta \n Eggplant Salad \n Lentil Burger \n Smoked Salmon \n Kofta Burger \n Turkish Kebab \n Fries \n Buttered Rice \n Bread Sticks \n Pita Pocket \n Lentil Soup \n Greek Salad \n Rice Pilaf \n Baklava \n Tartufo \n Tiramisu \n Panna Cotta',
+  { name: 'Hummus', id: '1A' },
+  { name: 'Moutabal', id: '2B' },
+  { name: 'Falafel', id: '3C' },
+  { name: 'Marinated Olives', id: '4D' },
+  { name: 'Kofta', id: '5E' },
+  { name: 'Eggplant Salad', id: '6F' },
+  { name: 'Lentil Burger', id: '7G' },
+  { name: 'Smoked Salmon', id: '8H' },
+  { name: 'Kofta Burger', id: '9I' },
+  { name: 'Turkish Kebab', id: '10J' },
+  { name: 'Fries', id: '11K' },
+  { name: 'Buttered Rice', id: '12L' },
+  { name: 'Bread Sticks', id: '13M' },
+  { name: 'Pita Pocket', id: '14N' },
+  { name: 'Lentil Soup', id: '15O' },
+  { name: 'Greek Salad', id: '16Q' },
+  { name: 'Rice Pilaf', id: '17R' },
+  { name: 'Baklava', id: '18S' },
+  { name: 'Tartufo', id: '19T' },
+  { name: 'Tartufo', id: '20U' },
+  { name: 'Tiramisu', id: '21V' },
+  { name: 'Panna Cotta', id: '22W' },
 ];
+
+const Item = () => (
+  <View style={menuStyles.innerContainer}>
+    <Text style={menuStyles.itemText}>{name}</Text>
+  </View>
+);
+
 export default function MenuItems() {
+  const renderItem = ({ item }) => <Item name={item.name} />;
+
   return (
     <View style={menuStyles.container}>
       <ScrollView
-        // indicatorStyle={'white'}
-        // bounces={true}
         minimumZoomScale={1}
         maximumZoomScale={2}
         bouncesZoom={true}
@@ -23,7 +51,11 @@ export default function MenuItems() {
         style={menuStyles.innerContainer}
       >
         <Text style={menuStyles.headerText}>View Menu</Text>
-        <Text style={menuStyles.itemText}>{menuItemsToDisplay[0]}</Text>
+        <FlatList
+          data={menuItemsToDisplay}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+        ></FlatList>
       </ScrollView>
     </View>
   );
@@ -36,12 +68,15 @@ const menuStyles = StyleSheet.create({
   innerContainer: {
     padding: 40,
     backgroundColor: green,
+    paddingHorizontal: 40,
+    paddingVertical: 20,
   },
   headerText: {
     color: 'white',
     fontSize: 40,
     flexWrap: 'wrap',
     fontWeight: 'bold',
+    textAlign: 'center',
   },
   itemText: { color: yellow, fontSize: 36 },
 });
